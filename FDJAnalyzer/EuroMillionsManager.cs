c425a41010290ab322eb26b4e079963f7e17cbdd
guizardremy34 <guizardremy34@gmail.com>
2024-01-21 18:12:09 +0100

    update

--- FDJAnalyzer/EuroMillionsManager.cs
+++ FDJAnalyzer/EuroMillionsManager.cs
@@ -13,7 +13,7 @@ namespace FDJAnalyzer
     internal class EuroMillionsManager
     {
         public static string[] DateTypes = { "Toutes", "Le", "Entre", "Après", "Avant" };
-        private static string base_link_api = "http://217.160.50.199/api/euro_millions/draws";
+        private static string base_link_api = "http://127.0.0.1:4000/api/euro_millions/draws";
 
         public async Task<Result> GetResult(Parameters? parameters)
         {
@@ -128,110 +128,6 @@ namespace FDJAnalyzer
             public int[]? stars { get; set; }
         }
 
-        /*public async Task<Result> getResults(Parameters? parameters = null)
-        {
-
-            if(draws != null)
-            {
-                for(int i = 0; i < draws.Count; i++)
-                {
-                    Draw draw = draws[i];
-                    if(draw.balls != null)
-                    {
-                        for (int b = 0; b < draw.balls.Length; b++)
-                        {
-                            if (!result.ballsStats.count.ContainsKey(draw.balls[b]))
-                            {
-                                result.ballsStats.count.Add(draw.balls[b], 1);
-                            } else
-                            {
-                                result.ballsStats.count[draw.balls[b]]++;
-                            }
-                            result.ballsStats.total++;
-                        }
-                    }
-                    if (draw.stars != null)
-                    {
-                        for (int b = 0; b < draw.stars.Length; b++)
-                        {
-                            if (!result.starsStats.count.ContainsKey(draw.stars[b]))
-                            {
-                                result.starsStats.count.Add(draw.stars[b], 1);
-                            }
-                            else
-                            {
-                                result.starsStats.count[draw.stars[b]]++;
-                            }
-                            result.starsStats.total++;
-                        }
-                    }
-                }
-                foreach (KeyValuePair<int, int> count in result.ballsStats.count)
-                {
-                    float per = (float)count.Value / result.ballsStats.total;
-                    result.ballsStats.per.Add(count.Key, per);
-                }
-                List<KeyValuePair<int, float>> ballsPerList = result.ballsStats.per.ToList();
-                ballsPerList.Sort(delegate (KeyValuePair<int, float> x, KeyValuePair<int, float> y)
-                {
-                    return x.Value.CompareTo(y.Value);
-                });
-                for(int i = 0; i < 5; i++)
-                {
-                    result.ballsStats.min.Add(ballsPerList[i].Value);
-                }
-                ballsPerList.Reverse();
-                for (int i = 0; i < 5; i++)
-                {
-                    result.ballsStats.max.Add(ballsPerList[i].Value);
-                }
-
-                foreach (KeyValuePair<int, int> count in result.starsStats.count)
-                {
-                    result.starsStats.per.Add(count.Key, (float)count.Value / result.starsStats.total);
-                }
-                List<KeyValuePair<int, float>> starsPerList = result.starsStats.per.ToList();
-                starsPerList.Sort(delegate (KeyValuePair<int, float> x, KeyValuePair<int, float> y)
-                {
-                    return x.Value.CompareTo(y.Value);
-                });
-                for (int i = 0; i < 2; i++)
-                {
-                    result.starsStats.min.Add(starsPerList[i].Value);
-                }
-                starsPerList.Reverse();
-                for (int i = 0; i < 2; i++)
-                {
-                    result.starsStats.max.Add(starsPerList[i].Value);
-                }
-            }
-
-            result.draws = draws;
-            return result;
-        }
-        public class Result
-        {
-            public List<Draw>? draws { get; set; }
-            public BallsStats ballsStats = new BallsStats();
-            public BallsStats starsStats = new BallsStats();
-            public class BallsStats
-            {
-                public IDictionary<int, int> count = new Dictionary<int, int>();
-                public IDictionary<int, float> per = new Dictionary<int, float>();
-                public List<float> min = new List<float>();
-                public List<float> max = new List<float>();
-                public int total = 0;
-            }
-            public class StarsStats
-            {
-                public IDictionary<int, int> count = new Dictionary<int, int>();
-                public IDictionary<int, float> per = new Dictionary<int, float>();
-                public List<float> min = new List<float>();
-                public List<float> max = new List<float>();
-                public int total = 0;
-            }
-
-        }*/
     }
 
 
